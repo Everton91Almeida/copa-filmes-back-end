befd149ddb3353e8a98fbb4fe2c09856ec4547b2
Everton91Almeida <everton91.almeida@gmail.com>
2020-01-24 23:34:56 -0300

    Testes da entidade filme

---
+++ CopaFilmes.Test/Domain/Filme/FilmeFixture.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using CopaFilmes.Domain.Entity;
+using System;
+using Xunit;
+
+namespace CandidateTesting.EvertonRodrigoLimaDeAlmeida.Test
+{
+    public class FilmeFixture : IDisposable
+    {
+
+        public Filme CreteWinnerFilme() =>
+            new Faker<Filme>("pt_BR")
+            .RuleFor(c => c.Titulo, f => f.Lorem.Word())
+            .RuleFor(c => c.Nota, f => f.Random.Float(9.5f, 10f))
+            .Generate();
+
+        public Filme CreteLoserFilme() =>
+            new Faker<Filme>("pt_BR")
+            .RuleFor(c => c.Titulo, f => f.Lorem.Word())
+            .RuleFor(c => c.Nota, f => f.Random.Float(0.5f, 1f))
+            .Generate();
+
+        public Filme CreteDrawWinnerFilme() =>
+            new Faker<Filme>("pt_BR")
+            .RuleFor(c => c.Titulo, f => $"A{f.Lorem.Word()}")
+            .RuleFor(c => c.Nota, f => 10)
+            .Generate();
+
+        public Filme CreteDrawLoserFilme() =>
+            new Faker<Filme>("pt_BR")
+            .RuleFor(c => c.Titulo, f => $"B{f.Lorem.Word()}")
+            .RuleFor(c => c.Nota, f => 10)
+            .Generate();
+
+
+        public void Dispose() =>
+            GC.SuppressFinalize(this);
+    }
+
+    [CollectionDefinition(nameof(FilmeCollection))]
+    public class FilmeCollection : ICollectionFixture<FilmeFixture> { }
+}
---
+++ CopaFilmes.Test/Domain/Filme/FilmeTest.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace CandidateTesting.EvertonRodrigoLimaDeAlmeida.Test
+{
+    [Collection(nameof(FilmeCollection))]
+    public class FilmeTest
+    {
+        private readonly FilmeFixture ServiceFixture;
+
+        public FilmeTest(FilmeFixture serviceFixture) =>
+            ServiceFixture = serviceFixture;
+
+        [Fact(DisplayName = "Check winner")]
+        [Trait("Entity", "Filme")]
+        public void Filme_GetWinner_ReturnWinner()
+        {
+            var winner = ServiceFixture.CreteWinnerFilme();
+            var loser = ServiceFixture.CreteLoserFilme();
+
+            var result = winner.GetWinner(loser);
+
+            Assert.Equal(winner, result);
+        }
+
+        [Fact(DisplayName = "Check winner when draw")]
+        [Trait("Entity", "Filme")]
+        public void Filme_GetWinner_ReturnWinnerByEqualRating()
+        {
+            var winner = ServiceFixture.CreteDrawWinnerFilme();
+            var loser = ServiceFixture.CreteDrawLoserFilme();
+
+            var result = winner.GetWinner(loser);
+
+            Assert.Equal(winner, result);
+        }        
+    }
+}
